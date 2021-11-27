a3b55ebdbfbc1f0bde6351f4e0acd3c93a692eaf
root <aqil@intermediatech.id>
2021-11-27 21:42:05 +0700

    node_modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Halo, Selamat siang saat ini kita sedang belajar CI/CD TI-3H</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
